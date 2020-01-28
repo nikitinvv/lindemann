BUG: Fix computation of angles

preprocess.py
@@ -5,9 +5,10 @@ import h5py
 import sys
 
 ##################################### Inputs #########################################################################
-file_name = '/local/data/2020-01/vnikitin/Continuous_1B_8400eV_100ms_2_1150.h5'
-sino_start = 0
-sino_end = 1024
+file_name = '/data/staff/tomograms/viknik/Lindemann/Continuous_1B_8400eV_100ms_2_1150.h5'
+#file_name = '/local/data/2020-01/vnikitin/Continuous_1B_8400eV_100ms_2_1150.h5'
+sino_start = 600
+sino_end = 600+128
 flat_field_norm = True
 flat_field_drift_corr = True  # Correct the intensity drift
 remove_rings = True
@@ -37,7 +38,7 @@ if __name__ == "__main__":
     prj, flat, dark, theta = dxchange.read_aps_32id(
         file_name, sino=(sino_start, sino_end))
     # set angles and dark field        
-    theta = np.arange(0,prj.shape[0])*theta[1]*180/np.pi
+    theta = np.arange(0,prj.shape[0])*theta[1]
     dark *= 0
     # preprocess
     prj = preprocess_data(prj, flat, dark, FF_norm=flat_field_norm, remove_rings=remove_rings,

rec.py
@@ -47,6 +47,7 @@ def myplot(u, psi, flow):
         os.makedirs('tmp'+'_'+str(ntheta)+'/')
     plt.savefig('tmp'+'_'+str(ntheta)+'/flow'+str(k))
     plt.close()
+    print(np.linalg.norm(flow))
 
 
 def update_penalty(psi, h, h0, rho):
@@ -61,8 +62,9 @@ def update_penalty(psi, h, h0, rho):
 
 if __name__ == "__main__":
 
-    data = np.load('prj.npy').astype('float32')#[0:2010]
-    theta = np.load('theta.npy').astype('float32')#[0:2010]
+    data = np.load('prj.npy').astype('float32')[0:1005]
+    theta = np.load('theta.npy').astype('float32')[0:1005]
+    print(theta)
     # Model parameters
     [ntheta,nz,n] = data.shape
     print(data.shape)
@@ -74,9 +76,7 @@ if __name__ == "__main__":
     psi = data.copy()
     lamd = np.zeros([ntheta, nz, n], dtype='complex64')
     flow = np.zeros([ntheta, 2], dtype='float32')
-    # optical flow parameters
-    pars = [0.5, 1, 256, 4, 5, 1.1, 4]
-
+   
     # ADMM solver
     with tc.SolverTomo(theta, ntheta, nz, n, pnz, center) as tslv:
         # ucg = tslv.cg_tomo_batch2(data, u, 8)
@@ -87,10 +87,10 @@ if __name__ == "__main__":
             h0 = psi
             for k in range(niter):
                 # registration
-                flow = dslv.registration_shift_batch(psi, data, flow)
+                flow = dslv.registration_shift_batch(psi, data, 1)
                 
                 # deformation subproblem
-                psi = dslv.cg_deform(data, psi, flow, 4,
+                psi = dslv.cg_shift(data, psi, flow, 4,
                                      tslv.fwd_tomo_batch(u)+lamd/rho, rho)
                 # tomo subproblem
                 u = tslv.cg_tomo_batch(psi-lamd/rho, u, 4)
@@ -107,7 +107,7 @@ if __name__ == "__main__":
                     lagr[1] = np.sum(np.real(np.conj(lamd)*(h-psi)))
                     lagr[2] = rho*np.linalg.norm(h-psi)**2
                     lagr[3] = np.sum(lagr[0:3])
-                    print(k, pars[2], np.linalg.norm(flow), rho, lagr)
+                    print(k, np.linalg.norm(flow), rho, lagr)
                     dxchange.write_tiff_stack(
                         u.real,  'tmp'+'_'+str(ntheta)+'/rect'+str(k)+'/r', overwrite=True)
                     dxchange.write_tiff_stack(
@@ -116,4 +116,3 @@ if __name__ == "__main__":
                 # Updates
                 rho = update_penalty(psi, h, h0, rho)
                 h0 = h
-#                pars[2] -= 2