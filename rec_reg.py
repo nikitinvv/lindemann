f101a6bab55d5337a71dcb3a925e9e2b4b445faf
math-vrn <vnikitin90@gmail.com>
2020-01-30 16:39:56 +0100

    DEV: fix shift

--- rec_reg.py
+++ rec_reg.py
@@ -6,7 +6,7 @@ import deformcg as dc
 import sys
 import os
 import matplotlib.pyplot as plt
-
+outdir = '/data/staff/tomograms/viknik/lindemann'
 def myplot(u, psi, flow):
     [ntheta, nz, n] = psi.shape
 
@@ -62,15 +62,15 @@ def update_penalty(psi, h, h0, rho):
 
 if __name__ == "__main__":
 
-    data = np.load('prj.npy').astype('float32')[:,64:]#[0:6025]
+    data = np.load('prj.npy').astype('float32')#[:,64:96]#[0:6025]
     theta = np.load('theta.npy').astype('float32')#[0:2010]
     # Model parameters
     [ntheta,nz,n] = data.shape
     print(data.shape)
-    center = 148  # rotation center
+    center = 296 # rotation center
     pnz = 64  # number of slice partitions for simultaneous processing in tomography
-    niter = 32
-    alpha = 1e-9
+    niter = 256
+    alpha = 5e-6
     # initial guess
     u = np.zeros([nz, n, n], dtype='complex64')
     psi1 = data.copy()
@@ -78,7 +78,7 @@ if __name__ == "__main__":
     psi2 = np.zeros([3, nz, n, n], dtype='complex64')    
     lamd2 = np.zeros([3, nz, n, n], dtype='complex64')    
     flow = np.zeros([ntheta, 2], dtype='float32')
-    # for center in range(140,155):
+    # for center in range(590,610):
     #     print(center)
     #     with tc.SolverTomo(theta, ntheta, nz, n, pnz, center) as tslv:
     #         ucg = tslv.cg_tomo_batch(data, u, 32)
@@ -100,12 +100,14 @@ if __name__ == "__main__":
                 # registration
                 flow = dslv.registration_shift_batch(data, psi1, 1)     # note swap data and psi for deformation!!!           
                 # deformation subproblem
-                psi1 = dslv.cg_shift(data, psi1, flow, 1,
+                psi1 = dslv.cg_shift(data, psi1, flow, 2,
                                      tslv.fwd_tomo_batch(u)+lamd1/rho1, rho1)
-                # regularization subproblem                                     
+                #psi1 = (dslv.apply_shift_batch(data, -flow) +
+                 #      rho1*tslv.fwd_tomo_batch(u)+lamd1)/(1+rho1)                     
+                # regularization subproblem          
                 psi2 = tslv.solve_reg(u,lamd2,rho2,alpha)    
                 # tomo subproblem
-                u = tslv.cg_tomo_batch_ext2(psi1-lamd1/rho1, u, 4, rho2/rho1, psi2-lamd2/rho2)
+                u = tslv.cg_tomo_batch_ext(psi1-lamd1/rho1, u, 4, rho2/rho1, psi2-lamd2/rho2)
 
                 h1 = tslv.fwd_tomo_batch(u)
                 h2 = tslv.fwd_reg(u)
@@ -114,7 +116,7 @@ if __name__ == "__main__":
                 lamd2 = lamd2+rho2*(h2-psi2)
                 # checking intermediate results
                 #myplot(u, psi, flow)
-                if(np.mod(k, 4) == 0):  # check Lagrangian
+                if(np.mod(k, 8) == 0):  # check Lagrangian
                     Tpsi = dslv.apply_shift_batch(psi1, flow)
                     lagr = np.zeros(7)
                     lagr[0] = np.linalg.norm(Tpsi-data)**2
@@ -131,10 +133,10 @@ if __name__ == "__main__":
                     #     # psi1.real, 'tmp2'+str(binning)+str(alpha)+'_'+str(ntheta)+'/psir'+str(k)+'/r',  overwrite=True)
 
                     dxchange.write_tiff_stack(
-                        u.real,  'tmp'+'_'+str(ntheta)+'_'+str(alpha)+'/rect'+str(k)+'/r', overwrite=True)
+                        u.real,  outdir+'/tmp'+'_'+str(ntheta)+'_'+str(alpha)+'/rect'+str(k)+'/r', overwrite=True)
                     dxchange.write_tiff_stack(
-                        psi1.real, 'tmp'+'_'+str(ntheta)+'_'+str(alpha)+'/psir'+str(k)+'/r',  overwrite=True)
-                    np.save('tmp'+'_'+str(ntheta)+'_'+str(alpha)+'/flow'+str(k), flow)                # Updates
+                        psi1.real, outdir+'/tmp'+'_'+str(ntheta)+'_'+str(alpha)+'/psir'+str(k)+'/r',  overwrite=True)
+                    np.save(outdir+'/tmp'+'_'+str(ntheta)+'_'+str(alpha)+'/flow'+str(k), flow)                # Updates
                 
                 rho1 = update_penalty(psi1, h1, h01, rho1)
                 rho2 = update_penalty(psi2, h2, h02, rho2)
